e5ba10a9a900909d82871b5272cf4433fc0b2806
mdeffy <160542791+mdeffy@users.noreply.github.com>
2024-03-03 20:07:28 +0200

    Add files via upload

---
+++ # user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask for user's name
+name = input("^Lungelo Zwane")
+
+# Ask for user's age
+age = input("24 years old ")
+
+# Ask for user's location
+location = input("Soweto,Johannesburg, South Africa")
+
+# Print personalized message
+print(f"Hello {"Lungelo Zwane"}, you are {"24 years old"} years old and live in {"Soweto Johannesburg,South Africa}.")
